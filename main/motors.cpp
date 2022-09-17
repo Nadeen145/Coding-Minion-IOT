990818f8788b3c17c3f90d85ef6c0a11042ccbce
Nadeen145 <49783776+Nadeen145@users.noreply.github.com>
2022-09-17 19:13:21 +0300

    Add files via upload

---
+++ main/motors.cpp
@@ -0,0 +1,143 @@
+
+#include "motors.h"
+
+L298NX2* motors;
+extern std::vector<turn> instructions;
+extern bool isFinish;
+extern bool isGameOver;
+
+void setupMotorPins() {
+  Serial.println("Starting motor setup pins");
+  
+  // With Enable pin to control speed
+  //                         motor A             motor B
+  //                   | --------------- | | --------------- |
+  motors = new L298NX2(EN_A, IN1_A, IN2_A, EN_B, IN1_B, IN2_B);
+  motors->setSpeedA(PWMA);
+  motors->setSpeedB(PWMB);
+}
+
+void turn_90_degree_left() {
+    Serial.println("Go Right (90 degree corner)");
+    motors->forwardA();
+    motors->stopB();
+    delay(CORNER_DELAY);
+    motors->forwardB();
+    delay(CORNER_DELAY_FORWARD);
+}
+
+void turn_90_degree_right() {
+    Serial.println("Go Left (90 degree corner)");
+    motors->stopA();
+    motors->forwardB();
+    delay(CORNER_DELAY);
+    motors->forwardA();
+    delay(CORNER_DELAY_FORWARD);
+}
+
+void setMotorsValueBySensors(int distance_front, int distance_right, int distance_left) {
+    if ((distance_front < MIN_DISTANCE_SIDE) || (distance_right > MAX_DISTANCE_SIDE) || (distance_left > MAX_DISTANCE_SIDE) { // The minion in front of a wall or in a conjunction
+        turn current = instructions.back();
+        instructions.pop_back();
+
+        switch (current) {
+            case(Right_A): color_A(); current = Right; break;
+            case(Right_B): color_B(); current = Right; break;
+            case(Right_C): color_C(); current = Right; break;
+            case(Right_D): color_D(); current = Right; break;
+            case(Left_a): color_a(); current = Left; break;
+            case(Left_b): color_b(); current = Left; break;
+            case(Left_c): color_c(); current = Left; break;
+            case(Left_d): color_d(); current = Left; break;
+            case(Forward_E): color_E(); current = Forward; break;
+            case(Forward_e): color_e(); current = Forward; break;
+        }
+
+        if (current == Left) {
+            turn_90_degree_left();
+            delay(1500);
+            Serial.print("Corner front: ");
+            Serial.print(readDistanceFront());
+            Serial.print(" right: ");
+            Serial.print(readDistanceRight());
+            Serial.print(" left: ");
+            Serial.println(readDistanceLeft());
+        }
+        else if (current == Right) {
+            turn_90_degree_right();
+            delay(1500);
+            Serial.print("Corner front: ");
+            Serial.print(readDistanceFront());
+            Serial.print(" right: ");
+            Serial.print(readDistanceRight());
+            Serial.print(" left: ");
+            Serial.println(readDistanceLeft());
+        }
+        else if (current == Forward) {
+            motors->forward();
+            delay(CORNER_DELAY);
+            Serial.print("Corner front: ");
+            Serial.print(readDistanceFront());
+            Serial.print(" right: ");
+            Serial.print(readDistanceRight());
+            Serial.print(" left: ");
+            Serial.println(readDistanceLeft());
+        }
+
+    }
+    else if (distance_right < MIN_DISTANCE_SIDE) { // The minion is too close to the wall from the right side
+        Serial.println("Go Left");
+        // turn to the left
+        motors->forwardA();
+        motors->stopB();
+        delay(RIGHT_DELAY_CORRECTION);
+        motors->forwardB();
+        delay(RIGHT_DELAY_CORRECTION_FORWARD);
+    }
+    else if (distance_left < MIN_DISTANCE_SIDE) { // The minion is too close to the wall from the left side
+        Serial.println("Go Right");
+        // turn to the right
+        motors->stopA();
+        motors->forwardB();
+        delay(LEFT_DELAY_CORRECTION);
+        motors->forwardA();
+        delay(LEFT_DELAY_CORRECTION_FORWARD);
+    }
+    else { // The minion is ok, keep moving forward
+        Serial.println("Go Forward");
+        motors->forward();
+    }
+}
+
+void setMotorsValueBySensors(int distance_right, int distance_left) {
+    if ((distance_front < MIN_DISTANCE_SIDE) || (distance_right > MAX_DISTANCE_SIDE) || (distance_left > MAX_DISTANCE_SIDE) { // The minion in front of a wall or in a conjunction
+        Serial.println("Wrong Solution :(");
+        motors->stop();
+        // voice "Game Over!!!"
+        isFinish = false;
+        isGameOver = true;
+        color_hair();
+    }
+    if (distance_right < MIN_DISTANCE_SIDE) { // The minion is too close to the wall from the right side
+        Serial.println("Go Left");
+        // turn to the left
+        motors->forwardA();
+        motors->stopB();
+        delay(RIGHT_DELAY_CORRECTION);
+        motors->forwardB();
+        delay(RIGHT_DELAY_CORRECTION_FORWARD);
+    }
+    else if (distance_left < MIN_DISTANCE_SIDE) { // The minion is too close to the wall from the left side
+        Serial.println("Go Right");
+        // turn to the right
+        motors->stopA();
+        motors->forwardB();
+        delay(LEFT_DELAY_CORRECTION);
+        motors->forwardA();
+        delay(LEFT_DELAY_CORRECTION_FORWARD);
+    }
+    else { // The minion is ok, keep moving forward
+        Serial.println("Go Forward");
+        motors->forward();
+    }
+}
---
+++ main/motors.h
@@ -0,0 +1,43 @@
+#ifndef MOTORS_H
+#define MOTORS_H
+
+#include "barcode.h"
+#include "L298NX2.h"
+#include "distance_sensors.h"
+
+#include <Arduino.h>
+#include <vector>
+#include <string>
+
+#define MIN_DISTANCE_FRONT 200
+#define MIN_DISTANCE_SIDE 50
+#define MAX_DISTANCE_SIDE 300
+#define CORNER_DELAY 3815
+#define CORNER_DELAY_FORWARD 400
+#define RIGHT_DELAY_CORRECTION 250
+#define RIGHT_DELAY_CORRECTION_FORWARD 1000
+#define LEFT_DELAY_CORRECTION 250
+#define LEFT_DELAY_CORRECTION_FORWARD 1000
+
+// motor A is the left side motor
+// motor B is the right side motor
+
+// Motor A
+#define EN_A 14
+#define IN1_A 27
+#define IN2_A 26
+#define PWMA 240    // Value from 0 to 255
+
+// Motor B
+#define EN_B 25
+#define IN1_B 13
+#define IN2_B 12
+#define PWMB 210    // Value from 0 to 255
+
+void setupMotorPins();
+void turn_90_degree_right();
+void turn_90_degree_left();
+void setMotorsValueBySensors(int distance_front, int distance_right, int distance_left);
+void setMotorsValueBySensors(int distance_right, int distance_left);
+
+#endif
